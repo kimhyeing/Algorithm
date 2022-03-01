6984e7792ee5a6dffbcbe5a4be2a8ad5ad9aba7c
hyein <hyein2072@gmail.com>
2022-03-01 17:36:30 +0900

    [DP] 11053_가장 긴 증가하는 부분 수열

---
+++ baekjoon/baekjoon/11053.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int arr[1000];
+int dp[1000];
+
+int main() {
+	int n, res = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+	
+	fill_n(dp, n, 1);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < i; j++) {
+			if (arr[j] < arr[i])
+				dp[i] = max(dp[i], dp[j] + 1);
+		}
+		res = max(dp[i], res);
+	}
+	cout << res;
+	return 0;
+}
