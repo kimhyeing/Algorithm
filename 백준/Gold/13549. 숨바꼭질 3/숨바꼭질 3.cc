34aaecfbb7e6bd2263c9509f5ad3ac467a707fd6
hyein-kim <68214704+kimhyeing@users.noreply.github.com>
2023-12-19 16:43:11 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2544 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -6,44 +6,35 @@
 
 using namespace std;
 
-const int MAX = 1e6+1;
+const int MAX = 1e5+1;
 int n,k;
 int dist[MAX];
-
-struct cmp{
-    bool operator()(pair<int,int>&a, pair<int,int>&b) {
-        if(a.first == b.first) {
-            return abs(k-a.second)>abs(k-b.second);
-        }
-        return a.first>b.first;
-    }
-};
 
 int dijkstra() {
     int res = MAX;
     fill_n(dist, MAX,  MAX);
-    priority_queue<pair<int,int>, vector<pair<int,int> >, cmp>pq;
-    pq.push(make_pair(0,n));
-    while(!pq.empty()) {
-        int cost = pq.top().first;
-        int cur = pq.top().second;
-        pq.pop();
+    queue<pair<int,int> >q;
+    q.push(make_pair(0,n));
+    while(!q.empty()) {
+        int cost = q.front().first;
+        int cur = q.front().second;
+        q.pop();
         if(cur == k) {
             res = cost;
             break;
         }
 
-        if(cur < k && dist[cur+1] > cost+1) {
-            dist[cur+1] = cost+1;
-            pq.push(make_pair(cost+1, cur+1));
+        if(cur < k && cur <= MAX/2 && dist[cur*2] > cost) {
+            dist[cur*2] = cost;
+            q.push(make_pair(cost, cur*2));
         }
         if(cur > 0 && dist[cur-1] > cost+1) {
             dist[cur-1] = cost+1;
-            pq.push(make_pair(cost+1, cur-1));
+            q.push(make_pair(cost+1, cur-1));
         }
-        if(cur < k && dist[cur*2] > cost) {
-            dist[cur*2] = cost;
-            pq.push(make_pair(cost, cur*2));
+        if(cur < k && dist[cur+1] > cost+1) {
+            dist[cur+1] = cost+1;
+            q.push(make_pair(cost+1, cur+1));
         }
     }
     return res;
