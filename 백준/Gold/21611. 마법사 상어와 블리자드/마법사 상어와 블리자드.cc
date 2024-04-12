9f3c796a6b297585407f7650d29cd829dbd799bb
hyein-kim <68214704+kimhyeing@users.noreply.github.com>
2024-04-12 21:04:23 +0900

    [Gold I] Title: 마법사 상어와 블리자드, Time: 8 ms, Memory: 2172 KB -BaekjoonHub

--- 백준/Gold/21611. 마법사 상어와 블리자드/마법사 상어와 블리자드.cc
+++ 백준/Gold/21611. 마법사 상어와 블리자드/마법사 상어와 블리자드.cc
@@ -1,252 +1,182 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include<cstring>
+#include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
 int n, m, sx, sy;
-int arr[52][52];
-int posx[4] = { 0,1,0,-1 };
-int posy[4] = { -1,0,1,0 };
-int res[4];
-bool boombable;
-vector<pair<int, int>>inst;
+int dirx[4] = { -1,1,0,0 };
+int diry[4] = { 0,0,-1,1 };
+const int UP = 0, DOWN = 1, LEFT = 2, RIGHT = 3;
+int arr[50][50];
+pair<int, int>mlist[100];
+int score[3];
+vector<pair<int, int>>root;
 
 void input() {
 	cin >> n >> m;
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
 			cin >> arr[i][j];
 		}
 	}
 	int d, s;
 	for (int i = 0; i < m; i++) {
 		cin >> d >> s;
-		if (d == 1) d = 3;
-		else if (d == 2) d = 1;
-		else if (d == 3) d = 0;
-		else d = 2;
-		inst.push_back(make_pair(d, s));
+		mlist[i] = make_pair(d-1, s);
 	}
-	sx = sy = (n + 1) / 2;
+	sx = sy = n / 2;
+}
+
+int nextDir(int d) {
+	if (d == LEFT) return DOWN;
+	else if (d == DOWN) return RIGHT;
+	else if (d == RIGHT) return UP;
+	else return LEFT;
+}
+
+void setRoot() {
+	int x = sx, y = sy, cur_dist = 0, dist = 1, dir = LEFT, cnt = 0;
+	bool isIce = false;
+	while (x != 0 || y != 0) {
+		x += dirx[dir];
+		y += diry[dir];
+		root.push_back(make_pair(x, y));
+		cur_dist++;
+		if (cur_dist == dist) {
+			cur_dist = 0;
+			cnt++;
+			dir = nextDir(dir);
+		}
+		if (cnt == 2) {
+			cnt = 0;
+			dist++;
+		}
+	}
+}
+
+void bombItem(int x, int y) {
+	score[arr[x][y] - 1]++;
+	arr[x][y] = 0;
+}
+
+int getResult() {
+	int result = 0;
+	for (int i = 1; i <= 3; i++) {
+		result += (i * score[i - 1]);
+	}
+	return result;
+}
+
+void ice(int d, int s) {
+	int x = sx, y = sy;
+	for (int i = 0; i < s; i++) {
+		x += dirx[d];
+		y += diry[d];
+		if (x < 0 || y < 0 || x >= n || y >= n || !arr[x][y]) break;
+		arr[x][y] = 0;
+	}
 }
 
 void move() {
-	int curx = sx + posx[0];
-	int cury = sy + posy[0];
-	int curd = 0;
-	int count = 1;
-	int dis = 1;
-	int newx, newy, newd, new_count, new_dis;
-	bool init = false;
-
-	while (true) {
-		while (arr[curx][cury] != 0) {
-			if (curx == 1 && cury == 1) {
-				return;
-			}
-			if (count == dis) {
-				if (curd == 1 || curd == 3)
-					dis++;
-				count = 0;
-				curd = (curd + 1) % 4;
-			}
-			curx = curx + posx[curd];
-			cury = cury + posy[curd];
-			count++;
+	int iceIdx = -1, x, y, iceCount = 0, preX, preY;
+	for (int i = 0; i < root.size(); i++) {
+		x = root[i].first;
+		y = root[i].second;
+		if (arr[x][y] == 0) {
+			iceCount++;
 		}
-
-		if (!init) {
-			newx = curx;
-			newy = cury;
-			new_count = count;
-			new_dis = dis;
-			newd = curd;
-			init = true;
+		else if ((i - iceCount) >= 0){
+			preX = root[i - iceCount].first;
+			preY = root[i - iceCount].second;
+			swap(arr[preX][preY], arr[x][y]);
 		}
-		while (arr[newx][newy] == 0) {
-			if (newx == 1 && newy == 1) {
-				return;
-			}
-			if (new_count == new_dis) {
-				if (newd == 1 || newd == 3)
-					new_dis++;
-				new_count = 0;
-				newd = (newd + 1) % 4;
-			}
-			newx = newx + posx[newd];
-			newy = newy + posy[newd];
-			new_count++;
-		}
-
-		swap(arr[curx][cury], arr[newx][newy]);
 	}
 }
 
-void bomb() {
-	int curx = sx + posx[0];
-	int cury = sy + posy[0];
-	int curd = 0;
-	int count = 1;
-	int dis = 1;
-	int newx, newy, newd, new_count, new_dis;
-	bool init = false;
-	int boom = 0, ball_num;
-
-	newx = curx;
-	newy = cury;
-	new_count = count;
-	new_dis = dis;
-	newd = curd;
-	while (true) {
-		boom = 0;
-		if (curx == 1 && cury == 1) {
-			return;
+int bomb() {
+	int bombCount = 0;
+	int num = -1, cnt = 0, x, y, tempx, tempy;
+	for (int i = 0; i < root.size(); i++) {
+		x = root[i].first;
+		y = root[i].second;
+		if (arr[x][y] != num && cnt < 4) {
+			num = arr[x][y];
+			cnt = 1;
 		}
-		if (curx < 1 || cury < 1) {
-			return;
-		}
+		else if (arr[x][y] != num && cnt >= 4) {
+			bombCount++;
+			for (int j = (i - 1); j > (i - 1 - cnt); j--) {
+				tempx = root[j].first;
+				tempy = root[j].second;
 
-		ball_num = arr[curx][cury];
-		while (arr[newx][newy] == ball_num) {
-			if (newx < 1 || newy < 1) {
-				return;
+				bombItem(tempx, tempy);
 			}
-			boom++;
-
-			if (new_count == new_dis) {
-				if (newd == 1 || newd == 3)
-					new_dis++;
-				new_count = 0;
-				newd = (newd + 1) % 4;
-			}
-			newx = newx + posx[newd];
-			newy = newy + posy[newd];
-			new_count++;
-		}
-
-		if (boom >= 4) {
-			boombable = true;
-			while (curx != newx || cury != newy) {
-				arr[curx][cury] = 0;
-				if (curx == 1 && cury == 1) {
-					return;
-				}
-				if (count == dis) {
-					if (curd == 1 || curd == 3)
-						dis++;
-					count = 0;
-					curd = (curd + 1) % 4;
-				}
-				curx = curx + posx[curd];
-				cury = cury + posy[curd];
-				count++;
-				res[ball_num]++;
-			}
+			num = arr[x][y];
+			cnt = 1;
 		}
 		else {
-			curx = newx;
-			cury = newy;
-			count = new_count;
-			dis = new_dis;
-			curd = newd;
+			cnt++;
 		}
 	}
+	return bombCount;
 }
 
 void change() {
-	int curx = sx + posx[0];
-	int cury = sy + posy[0];
-	int curd = 0;
-	int count = 1;
-	int dis = 1;
-	int ball_count = 0, ball_num;
-
-	vector<int>new_arr;
-	while (true) {
-		ball_count = 0;
-		if (curx == 1 && cury == 1) {
-			break;
+	vector<int>newItem;
+	int num, cnt = 0, x, y;
+	for (int i = 0; i < root.size(); i++) {
+		x = root[i].first;
+		y = root[i].second;
+		if (i == 0) {
+			num = arr[x][y];
 		}
-		if (arr[curx][cury] == 0)
-			break;
-
-		ball_num = arr[curx][cury];
-		while (arr[curx][cury] == ball_num) {
-			ball_count++;
-			if (curx == 1 && cury == 1) {
-				break;
-			}
-			if (count == dis) {
-				if (curd == 1 || curd == 3)
-					dis++;
-				count = 0;
-				curd = (curd + 1) % 4;
-			}
-			curx = curx + posx[curd];
-			cury = cury + posy[curd];
-			count++;
+		if (arr[x][y] != num) {
+			newItem.push_back(cnt);
+			newItem.push_back(num);
+			num = arr[x][y];
+			cnt = 1;
+			if (arr[x][y] == 0) break;
 		}
-		if (new_arr.size() >= (n * n - 1))
-			break;
-		new_arr.push_back(ball_count);
-		if (new_arr.size() >= (n * n - 1))
-			break;
-		new_arr.push_back(ball_num);
-		if (new_arr.size() >= (n * n - 1))
-			break;
+		else {
+			cnt++;
+		}
 	}
-
-	curx = sx + posx[0];
-	cury = sy + posy[0];
-	curd = 0;
-	count = 1;
-	dis = 1;
-	memset(arr, 0, sizeof(arr));
-	for (int i = 0; i < new_arr.size(); i++) {
-		arr[curx][cury] = new_arr[i];
-		if (curx == 1 && cury == 1) {
-			return;
+	
+	for (int i = 0; i < root.size(); i++) {
+		x = root[i].first;
+		y = root[i].second;
+		if (i >= (newItem.size())) {
+			arr[x][y] = 0;
 		}
-		if (count == dis) {
-			if (curd == 1 || curd == 3)
-				dis++;
-			count = 0;
-			curd = (curd + 1) % 4;
+		else {
+			arr[x][y] = newItem[i];
 		}
-		curx = curx + posx[curd];
-		cury = cury + posy[curd];
-		count++;
 	}
+	
 }
-int main() {
-	input();
-	int d, s, newx, newy;
-
+void solve() {
+	int d, s;
 	for (int i = 0; i < m; i++) {
-		d = inst[i].first;
-		s = inst[i].second;
-		for (int j = 1; j <= s; j++) {
-			newx = sx + j * posx[d];
-			newy = sy + j * posy[d];
-			arr[newx][newy] = 0;
-		}
-		
+		d = mlist[i].first;
+		s = mlist[i].second;
+		ice(d, s);
 		move();
-		boombable = true;
-		while (boombable) {
-			boombable = false;
-			bomb();
+		while (bomb()) {
 			move();
 		}
 		change();
 	}
-	int boom_sum = 0;
-	for (int i = 1; i <= 3; i++) {
-		boom_sum += (i * res[i]);
-	}
-	cout << boom_sum;
+}
 
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	input();
+	setRoot();
+	solve();
+	cout << getResult();
 	return 0;
 }
