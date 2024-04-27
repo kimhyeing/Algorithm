9a09e35178561d63794746569d74e62d25e44fcf
hyein-kim <68214704+kimhyeing@users.noreply.github.com>
2024-04-27 21:31:56 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -1,11 +1,13 @@
-select year(b.sales_date) as y, month(b.sales_date) as m, count(distinct b.user_id), 
-    round(count(distinct b.user_id)/(select count(distinct user_info.user_id)
-                              from user_info
-                              where year(user_info.joined) = 2021
-                              group by year(user_info.joined)),1)
-from user_info as a join online_sale as b
-on a.user_id = b.user_id
-where year(a.joined) = 2021
-group by y, m
-order by m
-
+select t.year, t.month, count(distinct t.user_id) as PUCHASED_USERS, round(count(distinct t.user_id)/avg(c.all_users), 1)
+from
+    (select a.user_id, b.sales_amount, year(b.sales_date) as year, month(b.sales_date) as month
+    from (select user_id
+        from user_info
+        where year(joined)=2021) as a join online_sale as b
+        on a.user_id = b.user_id) as t, 
+        (select count(distinct user_id) as all_users
+        from user_info
+        where year(joined)=2021) as c
+group by t.year, t.month
+order by t.year, t.month
+    
